dc794ca101bef95c42215cb14535865bfe72f8dc
Sohaib Sharih <sohaib.sharih@gmail.com>
2020-11-16 17:52:29 +0500

    first line

---
+++ Calculator01.js
@@ -0,0 +1,26 @@
+var a;
+a = 10;
+var b;
+b = 5;
+
+var result = a + b;
+console.log(result);
+
+result = a / b;
+console.log(result);
+
+result = a - b;
+console.log(result);
+
+result = a * b;
+console.log(result);
+
+a = ++a;
+result = ++a;
+console.log(result);
+
+b = --b;
+result = --b;
+console.log(result);
+
+
